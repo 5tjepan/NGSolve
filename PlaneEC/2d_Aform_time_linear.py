437b43828203ba5ae0cebf345d782bde1ef362f7
Stjepan <stjepan.frljic@gmail.com>
2024-10-17 14:15:42 +0200

    stabilna verzija 2d_Aform_time_linear.py. Sprema B i J u proizvoljnom vrem range uz zadani samplestep. Vizualizira B i J u proizvoljnoj tocki.

--- PlaneEC/2d_Aform_time_linear.py
+++ PlaneEC/2d_Aform_time_linear.py
@@ -3,19 +3,14 @@ from netgen.occ import *
 
 outer= Circle((0,0), 0.2).Face()
 outer.edges.name = 'rub'
-
-inner = MoveTo(-0.08,-0.08).Line(0.16,0.0).Line(0,0.16).Line(-0.16,0).Close().Face()
-#inner = MoveTo(-0.8,-0.8).Line(1.6,-0.1).Line(0,1.7).Line(-1.6,-0.2).Close().Face()
-
-
+brid=0.16
+inner = MoveTo(-0.08,-0.08).Line(brid,0.0).Line(0,brid).Line(-brid,0).Close().Face()
 inner.edges.name="interface"
 inner.faces.maxh=0.01
 outer = outer - inner
-
 inner.faces.name="inner"
 inner.faces.col = (1, 1, 0) 
 outer.faces.name="outer"
-
 
 geo = Glue([inner, outer])
 #Draw(OCCGeometry(geo));
@@ -32,10 +27,9 @@ time = 0.0
 omega=2*pi*1
 dt = 2*pi/omega /40
 
-
 mu0 = 1.257e-6
 rel = 200
-sigma = 2e3
+sigma = 2e4
 sig = mesh.MaterialCF({ "inner" : 1 }, default=None)
 
 a = BilinearForm(fes, symmetric=False)
@@ -72,43 +66,49 @@ gfu.Set(bc, definedon=mesh.Boundaries('rub'))
 #Draw(gfu, mesh, "gfu_bnd")
 
 
-def TimeSteppingSin(invmstar, initial_cond = None, t0 = 0, tend = 2):
+Blist=[]
+Jlistoftuple=[]
+time_axis=[]
+
+def TimeStepping(invmstar, initial_cond = None, t0 = 0, tend = 2,
+                     range=(0.8,0.9), samplestep=2):
     if initial_cond:
         gfu.Set(initial_cond)
     cnt = 0; time = t0
-    #sample_int = int(floor(tend / dt / nsamples)+1)
     gfuD = GridFunction(gfu.space)
     DgfuDt= GridFunction(gfu.space) 
     gfuPrev=GridFunction(gfu.space)
+    gfuPrev.vec.data=gfu.vec
     
     gfut = GridFunction(gfu.space,multidim=0)
     Jgfut = GridFunction(gfu.space,multidim=0)
-    
     gfut.AddMultiDimComponent(gfu.vec)
+    Jgfut.AddMultiDimComponent(gfu.vec)
     while time < tend - 0.5 * dt:
         t.Set(time)
         #print(bc(mesh(0.198, 0.0)))
-        gfuD.Set(bc,definedon=mesh.Boundaries('rub'))
+        gfuD.Set(bc,definedon=mesh.Boundaries('rub')) #vrem. ovisan bc (t.Set(time))
         res = m.mat * gfu.vec - mstar *gfuD.vec
         gfu.vec.data = gfuD.vec + invmstar * res #uoci znak "=" umjesto "+="
         DgfuDt.vec.data = (gfu.vec - gfuPrev.vec)
         gfuPrev.vec.data=gfu.vec
-        
-        period=2*pi/omega
-        if time>1.3*period and time<1.4*period :
+        Blist.append(curl(gfu)[0](mesh(0,0.49*brid)))
+        Jlistoftuple.append(DgfuDt(mesh(0,0.49*brid)))
+        #print(DgfuDt.Norm()(mesh(0,0.49*brid)))
+        time_axis.append(time)
+        if time>range[0]*(tend-t0) and time<range[1]*(tend-t0) and cnt%samplestep==0 :
             gfut.AddMultiDimComponent(gfu.vec)
             Jgfut.AddMultiDimComponent(DgfuDt.vec)
         cnt += 1; time = cnt * dt
         #print(cnt)
     return gfut, Jgfut
 
-
-gfut, Jgfut = TimeSteppingSin(invmstar, initial_cond=CF((0,0)),tend=2)
+gfut, Jgfut = TimeStepping(invmstar, initial_cond=CF((0,0)),tend=3,samplestep=3)
 
 
 ####....... P O S T P R O C E S I N G........
 ##
-comp=2
+comp=1
 
 gfu.vec.data=gfut.vecs[comp]
 B=curl(gfu)
@@ -119,14 +119,32 @@ Jgfu.vec.data=Jgfut.vecs[comp]
 J= Jgfu*sigma*(-1) * sig
 Draw(J, mesh, f"J_{comp}")
 
-
 #Pow=0.5*E*Conj(J) 
 #Peddy=Integrate(Pow, mesh, order=5)
 #print('Peddy',Peddy) 
 
 
-#alternativa:
-def TimeStepping(invmstar, initial_cond = None, t0 = 0, tend = 2,
+import matplotlib.pyplot as plt
+import sys
+sys.argv = ["fun"]
+
+#Blist=[elem[0] for elem in Blistoftuple]
+Jlist=[-1*sigma*elem[0] for elem in Jlistoftuple] #elem[0] je Jx komponenta
+
+plt.figure()
+
+plt.subplot(211)
+plt.plot(time_axis[1:],Blist[1:],'b')
+plt.grid(True)
+#...
+plt.subplot(212)
+plt.plot(time_axis[1:], Jlist[1:],'r--')
+plt.grid(True)
+plt.show()
+
+
+#alternativa samo za B:
+def TimeSteppingOld(invmstar, initial_cond = None, t0 = 0, tend = 2,
                       nsamples = 10):
     if initial_cond:
         gfu.Set(initial_cond)
@@ -148,5 +166,4 @@ def TimeStepping(invmstar, initial_cond = None, t0 = 0, tend = 2,
         #print(cnt)
     return gfut
 
-
-#gfut = TimeStepping(invmstar, initial_cond=CF((0,0)),tend=1)
+#gfut = TimeSteppingOld(invmstar, initial_cond=CF((0,0)),tend=1)
